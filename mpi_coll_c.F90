4244a5df9b8fac847482fa8ae1be035ea796d321
Jeff Hammond <jeff.science@gmail.com>
2022-10-07 17:29:30 +0300

    fix F2C part

--- mpi_coll_c.F90
+++ mpi_coll_c.F90
@@ -15,11 +15,7 @@ module mpi_coll_c
                                ierror_c) bind(C,name="C_MPI_Bcast")
             use iso_c_binding, only: c_int
             implicit none
-#ifdef __NVCOMPILER
-            class(*), dimension(..), intent(inout) :: buffer
-#else
-            type(*), dimension(..), intent(inout) :: buffer
-#endif
+            integer(kind=c_int), dimension(*), intent(inout) :: buffer
             integer(kind=c_int) :: count_c, datatype_c, root_c, comm_c, ierror_c
         end subroutine C_MPI_Bcast
     end interface
@@ -43,13 +39,8 @@ module mpi_coll_c
                                ierror_c) bind(C,name="C_MPI_Reduce")
             use iso_c_binding, only: c_int
             implicit none
-#ifdef __NVCOMPILER
-            class(*), dimension(..), intent(in)    :: input
-            class(*), dimension(..), intent(inout) :: output
-#else
-            type(*), dimension(..), intent(in)    :: input
-            type(*), dimension(..), intent(inout) :: output
-#endif
+            integer(kind=c_int), dimension(*), intent(in)    :: input
+            integer(kind=c_int), dimension(*), intent(inout) :: output
             integer(kind=c_int) :: count_c, datatype_c, op_c, root_c, comm_c, ierror_c
         end subroutine C_MPI_Reduce
     end interface
@@ -75,13 +66,8 @@ module mpi_coll_c
                                ierror_c) bind(C,name="C_MPI_Allreduce")
             use iso_c_binding, only: c_int
             implicit none
-#ifdef __NVCOMPILER
-            class(*), dimension(..), intent(in)    :: input
-            class(*), dimension(..), intent(inout) :: output
-#else
-            type(*), dimension(..), intent(in)    :: input
-            type(*), dimension(..), intent(inout) :: output
-#endif
+            integer(kind=c_int), dimension(*), intent(in)    :: input
+            integer(kind=c_int), dimension(*), intent(inout) :: output
             integer(kind=c_int) :: count_c, datatype_c, op_c, comm_c, ierror_c
         end subroutine C_MPI_Allreduce
     end interface
