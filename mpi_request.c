25671bf5d4cecb829c6b46e3e3f8725d240344ac
Jeff Hammond <jeff.science@gmail.com>
2022-10-10 17:31:28 +0300

    request_free

--- mpi_request.c
+++ mpi_request.c
@@ -16,3 +16,10 @@ void C_MPI_Request_get_status(const int * request_f, int * flag_f, MPI_Status st
     *ierror = MPI_Request_get_status(request, &flag, &status);
     *flag_f = flag;
 }
+
+void C_MPI_Request_free(int * request_f, int * ierror)
+{
+    MPI_Request request = MPI_Request_f2c(*request_f);
+    *ierror = MPI_Request_free(&request);
+    *request_f = MPI_Request_c2f(request);
+}
