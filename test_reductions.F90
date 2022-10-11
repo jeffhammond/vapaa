a8cb3098eae0d34de28a4a23646e0e2e6d4e27a6
Jeff Hammond <jeff.science@gmail.com>
2022-10-11 09:49:12 +0300

    test doubles too

--- test_reductions.F90
+++ test_reductions.F90
@@ -4,7 +4,8 @@ program test_reductions
     integer :: ierror
     integer :: i, me, np
     integer :: b
-    integer, allocatable :: x(:), y(:)
+    integer, allocatable :: ix(:), iy(:)
+    double precision, allocatable :: dx(:), dy(:)
 
     call MPI_Init(ierror)
 
@@ -19,19 +20,37 @@ program test_reductions
 
     do i=0,20
         b = 2**i
-        if (me.eq.0) print*,'b=',b
-        allocate( x(b) )
-        allocate( y(b) )
-        x =  1
-        y = -1
-        call MPI_Allreduce(x, y, b, MPI_INTEGER, MPI_SUM,  MPI_COMM_WORLD)
-        if (any(y.ne.np)) then
+        if (me.eq.0) print*,b,' integers'
+        allocate( ix(b) )
+        allocate( iy(b) )
+        ix =  1
+        iy = -1
+        call MPI_Allreduce(ix, iy, b, MPI_INTEGER, MPI_SUM,  MPI_COMM_WORLD)
+        if (any(iy.ne.np)) then
             print*,'an error has occurred'
-            print*,y
+            print*,iy
         endif
-        deallocate( x )
-        deallocate( y )
+        deallocate( ix )
+        deallocate( iy )
     enddo
+
+    do i=0,20
+        b = 2**i
+        if (me.eq.0) print*,b,' doubles'
+        allocate( dx(b) )
+        allocate( dy(b) )
+        dx =  1
+        dy = -1
+        call MPI_Allreduce(dx, dy, b, MPI_DOUBLE_PRECISION, MPI_SUM,  MPI_COMM_WORLD)
+        if (any(abs(dy-np).gt.tiny(0.0d0))) then
+            print*,'an error has occurred'
+            print*,dy
+        endif
+        deallocate( dx )
+        deallocate( dy )
+    enddo
+
+    if(me.eq.0) print*,'EVERYTHING IS OKAY'
 
     call MPI_Finalize(ierror)
 
