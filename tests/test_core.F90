5b3b709d94fa12ef8ed87da875d97d4297af441f
Jeff Hammond <jeff.science@gmail.com>
2022-11-25 11:37:03 +0200

    test initialized but finalized is messed up

--- tests/test_core.F90
+++ tests/test_core.F90
@@ -6,8 +6,17 @@ program test_core
     integer :: v, sv
     character(len=MPI_MAX_LIBRARY_VERSION_STRING) :: lib
     integer :: liblen
+    logical :: flags(3,2)
+
+    flags = .false.
+
+    call MPI_Initialized(flags(1,1))
+    !call MPI_Finalized(flags(1,2))
 
     call MPI_Init(ierror)
+
+    call MPI_Initialized(flags(2,1))
+    !call MPI_Finalized(flags(2,2))
 
     call MPI_Comm_rank(MPI_COMM_WORLD,me)
     call MPI_Comm_size(MPI_COMM_WORLD,np)
@@ -17,12 +26,17 @@ program test_core
     if (me.eq.0) print*,'MPI ',v,'.',sv
 
     call MPI_Get_library_version(lib, liblen)
-    if (me.eq.0) print*,'MPI library: ',lib
+    !if (me.eq.0) print*,'MPI library: ',lib
 
     call MPI_Comm_rank(MPI_COMM_SELF,me)
     call MPI_Comm_size(MPI_COMM_SELF,np)
     print*,'I am ',me,' of ',np,' of SELF'
 
     call MPI_Finalize(ierror)
+
+    call MPI_Initialized(flags(3,1))
+    !call MPI_Finalized(flags(3,2))
+
+    if (me.eq.0) print*,'init=',flags(:,1)!,' final=',flags(:,2)
 
 end program test_core
