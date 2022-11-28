cfba721fce182714ff4dc9c73f0b51acdd68fb3d
Jeff Hammond <jeff.science@gmail.com>
2022-11-28 09:58:15 +0200

    test of noncontig x noncontig

--- sandbox/vector_noncontig.F90
+++ sandbox/vector_noncontig.F90
@@ -10,19 +10,24 @@ program main
     call MPI_Comm_rank(MPI_COMM_WORLD,me)
     call MPI_Comm_size(MPI_COMM_WORLD,np)
 
+    if (me.eq.0) then
+        print*,'MPI_SUBARRAYS_SUPPORTED=',MPI_SUBARRAYS_SUPPORTED
+        print*,'MPI_ASYNC_PROTECTS_NONBLOCKING=',MPI_ASYNC_PROTECTS_NONBLOCKING
+    endif
+
     A = [i, i=1,100]
     B = 0
 
     call MPI_Type_vector(25,1,2,MPI_INTEGER,v)
     call MPI_Type_commit(v)
 
-    print*,'A=',A
+    if (me.eq.0) print*,'A=',A
 
     call MPI_Isend( A(1:100:1), 1, v, 1, 99, MPI_COMM_WORLD, r(1))
     call MPI_Irecv( B, 25, MPI_INTEGER, 0, 99, MPI_COMM_WORLD, r(2))
     call MPI_Waitall(2, r, MPI_STATUSES_IGNORE)
 
-    print*,'B=',A
+    if (me.eq.0) print*,'B=',B
 
     B = 0
 
@@ -30,7 +35,7 @@ program main
     call MPI_Irecv( B, 25, MPI_INTEGER, 0, 99, MPI_COMM_WORLD, r(2))
     call MPI_Waitall(2, r, MPI_STATUSES_IGNORE)
 
-    print*,'B=',A
+    if (me.eq.0) print*,'B=',B
 
     call MPI_Type_free(v)
 
