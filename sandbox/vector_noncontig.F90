63d00bf3593f15d31f23ac7e55ce5bb441d630cd
Jeff Hammond <jeff.science@gmail.com>
2022-11-28 09:56:23 +0200

    test of noncontig x noncontig

---
+++ sandbox/vector_noncontig.F90
@@ -0,0 +1,39 @@
+program main
+    use mpi_f08
+    implicit none
+    integer :: me, np, i
+    integer :: A(100), B(25)
+    type(MPI_Datatype) :: v
+    type(MPI_Request) :: r(2)
+
+    call MPI_Init()
+    call MPI_Comm_rank(MPI_COMM_WORLD,me)
+    call MPI_Comm_size(MPI_COMM_WORLD,np)
+
+    A = [i, i=1,100]
+    B = 0
+
+    call MPI_Type_vector(25,1,2,MPI_INTEGER,v)
+    call MPI_Type_commit(v)
+
+    print*,'A=',A
+
+    call MPI_Isend( A(1:100:1), 1, v, 1, 99, MPI_COMM_WORLD, r(1))
+    call MPI_Irecv( B, 25, MPI_INTEGER, 0, 99, MPI_COMM_WORLD, r(2))
+    call MPI_Waitall(2, r, MPI_STATUSES_IGNORE)
+
+    print*,'B=',A
+
+    B = 0
+
+    call MPI_Isend( A(1:100:2), 1, v, 1, 99, MPI_COMM_WORLD, r(1))
+    call MPI_Irecv( B, 25, MPI_INTEGER, 0, 99, MPI_COMM_WORLD, r(2))
+    call MPI_Waitall(2, r, MPI_STATUSES_IGNORE)
+
+    print*,'B=',A
+
+    call MPI_Type_free(v)
+
+    call MPI_Finalize()
+
+end program main
