55435964625f3a6f5a1f087e2e13679ff5ff4351
Jeff Hammond <jeff.science@gmail.com>
2022-11-28 11:07:26 +0200

    intent

--- source/mpi_error_c.F90
+++ source/mpi_error_c.F90
@@ -5,8 +5,9 @@ module mpi_error_c
                    bind(C,name="CFI_MPI_Error_string")
             use iso_c_binding, only: c_int, c_char
             implicit none
-            integer(kind=c_int) :: errorcode_c, resultlen_c, ierror_c
-            character(kind=c_char), dimension(:) :: string_c
+            integer(kind=c_int), intent(in) :: errorcode_c
+            integer(kind=c_int), intent(out) :: resultlen_c, ierror_c
+            character(kind=c_char), dimension(:), intent(out) :: string_c
         end subroutine CFI_MPI_Error_string
     end interface
 
@@ -15,7 +16,8 @@ module mpi_error_c
                    bind(C,name="C_MPI_Error_class")
             use iso_c_binding, only: c_int
             implicit none
-            integer(kind=c_int) :: errorcode_c, errorclass_c, ierror_c
+            integer(kind=c_int), intent(in) :: errorcode_c
+            integer(kind=c_int), intent(out) :: errorclass_c, ierror_c
         end subroutine C_MPI_Error_class
     end interface
 
