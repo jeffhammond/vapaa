1230eed89fe737e1c8c4937b313f4b48c80c8170
Jeff Hammond <jeff.science@gmail.com>
2022-12-27 17:17:55 +0200

    add some comments

--- source/mpi_win.c
+++ source/mpi_win.c
@@ -4,7 +4,7 @@
 #if 0
 static int C_MPI_TRANSLATE_WIN_ASSERT(int f)
 {
-    // all of the VAPAA constants are powers of two, so that bit logic works
+    // all of the VAPAA constants are powers of two, to ensure bit logic works
     int c = 0;
     if (f & VAPAA_MPI_NOCHECK  ) c |= MPI_MODE_NOCHECK;
     if (f & VAPAA_MPI_NOSTORE  ) c |= MPI_MODE_NOSTORE;
