c9d0f6d04b8bcaae80c7703d544273c907b50832
Jeff Hammond <jeff.science@gmail.com>
2023-01-02 10:31:35 +0200

    improve test

--- tests/test_thread.F90
+++ tests/test_thread.F90
@@ -1,6 +1,9 @@
 program main
+
     use mpi_f08
+
     implicit none
+
     integer :: requested = MPI_THREAD_MULTIPLE
     integer :: provided
     integer :: me
@@ -10,13 +13,18 @@ program main
     call MPI_Comm_rank(MPI_COMM_WORLD,me)
 
     if (me.eq.0) then
+
         print*,'Init_thread:',requested,provided
-    endif
 
-    call MPI_Query_thread(provided)
-
-    if (me.eq.0) then
+        call MPI_Query_thread(provided)
         print*,'Query:',provided
+
+        ! make sure all 4 thread levels are defined
+        print*,'MPI_THREAD_SINGLE     = ',MPI_THREAD_SINGLE
+        print*,'MPI_THREAD_FUNNELED   = ',MPI_THREAD_FUNNELED
+        print*,'MPI_THREAD_SERIALIZED = ',MPI_THREAD_SERIALIZED
+        print*,'MPI_THREAD_MULTIPLE   = ',MPI_THREAD_MULTIPLE
+
     endif
 
     call MPI_Finalize()
