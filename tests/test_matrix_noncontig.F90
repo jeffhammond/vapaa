b2f70f2faa35197199ec79f3cf30be79add68ca5
Jeff Hammond <jeff.science@gmail.com>
2023-01-02 14:45:22 +0200

    matrix test allocatable

--- tests/test_matrix_noncontig.F90
+++ tests/test_matrix_noncontig.F90
@@ -2,7 +2,11 @@ program main
     use mpi_f08
     implicit none
     integer :: me, np, i
-    integer :: A(20,20), B(10,10)
+#if 1
+    integer, allocatable :: A(:,:), B(:,:)
+#else
+    integer :: A(30,20), B(10,10)
+#endif
     type(MPI_Datatype) :: v
 
     call MPI_Init()
@@ -14,12 +18,14 @@ program main
         print*,'MPI_ASYNC_PROTECTS_NONBLOCKING=',MPI_ASYNC_PROTECTS_NONBLOCKING
     endif
 
-    A = reshape([(i, i = 1,400)],[20,20])
+#if 1
+    allocate( A(30,20), B(10,10) )
+#endif
+
+    A = reshape([(i, i = 1,size(A,1)*size(A,2))],[size(A,1),size(A,2)])
     if (me.eq.0) print*,'A=',A
-
     B = 0
-    call MPI_Bcast( A(2:20:2,2:20:2), 100, MPI_INTEGER, 0, MPI_COMM_WORLD )
-
+    call MPI_Bcast( A(3:30:3,2:20:2), 100, MPI_INTEGER, 0, MPI_COMM_WORLD )
     if (me.eq.0) print*,'B=',B
 
     call MPI_Finalize()
