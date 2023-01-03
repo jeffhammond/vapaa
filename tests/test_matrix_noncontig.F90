f53647cfa7e6ca589020275dff94df9c6a47e85e
Jeff Hammond <jeff.science@gmail.com>
2023-01-03 10:51:05 +0200

    cleanup noncontig tests

--- tests/test_matrix_noncontig.F90
+++ tests/test_matrix_noncontig.F90
@@ -2,12 +2,13 @@ program main
     use mpi_f08
     implicit none
     integer :: me, np, i
-#if 1
-    integer, allocatable :: A(:,:), B(:,:)
-#else
-    integer :: A(30,20), B(10,10)
-#endif
     type(MPI_Datatype) :: v
+#if 0
+    integer, allocatable :: A(:,:)
+    allocate( A(30,20) )
+#else
+    integer :: A(30,32)
+#endif
 
     call MPI_Init()
     call MPI_Comm_rank(MPI_COMM_WORLD,me)
@@ -18,15 +19,12 @@ program main
         print*,'MPI_ASYNC_PROTECTS_NONBLOCKING=',MPI_ASYNC_PROTECTS_NONBLOCKING
     endif
 
-#if 1
-    allocate( A(30,20), B(10,10) )
-#endif
-
     A = reshape([(i, i = 1,size(A,1)*size(A,2))],[size(A,1),size(A,2)])
+    !if (me.eq.0) print*,'A=[',A,']'
+    !if (me.eq.0) print*,'shape(A)=',shape(A)
+
+    call MPI_Bcast( A(1:30:1,1:20:3), 1, MPI_INTEGER, 0, MPI_COMM_WORLD )
     if (me.eq.0) print*,'A=',A
-    B = 0
-    call MPI_Bcast( A(3:30:3,2:20:2), 100, MPI_INTEGER, 0, MPI_COMM_WORLD )
-    if (me.eq.0) print*,'B=',B
 
     call MPI_Finalize()
 
