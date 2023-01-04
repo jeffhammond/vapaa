58816c4954b5d20b5dba642d42ed4d7d1a7f581d
Jeff Hammond <jeff.science@gmail.com>
2023-01-04 13:51:58 +0200

    non-cartesian submatrix

--- tests/test_matrix_noncontig.F90
+++ tests/test_matrix_noncontig.F90
@@ -45,6 +45,30 @@ program main
         endif
     endif
 
+    B = 0
+    call MPI_Isend( A(1:30:3,1:20:2), size(A(1:30:3,1:20:2))-1, MPI_INTEGER, me, 99, MPI_COMM_WORLD, r(1))
+    call MPI_Irecv( B, size(B), MPI_INTEGER, me, 99, MPI_COMM_WORLD, r(2))
+    call MPI_Waitall(2, r, MPI_STATUSES_IGNORE)
+    ! first check the omitted element, then set it to what it should be so the whole array check passes
+    if (B(10,10).ne.0) then
+        print*,'an error has occurred'
+        if (me .eq. 0) then
+            !write(*,'(a,600i4,a)') 'A=[',A,']'
+            write(*,'(a,100i4,a)') 'A[...]=[',A(1:30:3,1:20:2),']'
+            write(*,'(a,100i4,a)') 'B[ * ]=[',B,']'
+        endif
+    else
+        B(10,10) = A(28,19) 
+    endif
+    if (any(B.ne.A(1:30:3,1:20:2))) then
+        print*,'an error has occurred'
+        if (me .eq. 0) then
+            !write(*,'(a,600i4,a)') 'A=[',A,']'
+            write(*,'(a,100i4,a)') 'A[...]=[',A(1:30:3,1:20:2),']'
+            write(*,'(a,100i4,a)') 'B[ * ]=[',B,']'
+        endif
+    endif
+
     if (me.eq.0) then
         print*,'non-contiguous matrix support is okay'
     end if
