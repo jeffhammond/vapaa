b69f89913ae709d977c938a1bc134ca9bf62de3b
Jeff Hammond <jeff.science@gmail.com>
2023-01-14 13:16:36 +0200

    attr test

--- tests/test_attr.F90
+++ tests/test_attr.F90
@@ -4,11 +4,17 @@ program main
     integer :: me,ierror,e,c
     character(len=MPI_MAX_ERROR_STRING) :: s
     integer :: len
+    type(MPI_Datatype) :: v
 
     call MPI_Init(ierror)
     call MPI_Comm_rank(MPI_COMM_WORLD,me)
 
     if (me.eq.0) then
+
+        call MPI_Type_vector(26,1,2,MPI_CHARACTER,v)
+        call MPI_Type_set_name(v,'VAPAA: 26,1,2,MPI_CHAR')
+        call MPI_Type_commit(v)
+        call MPI_Type_free(v)
 
     endif
 
