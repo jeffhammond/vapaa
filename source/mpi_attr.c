62f6e3a03549acddc6b6e74e5aebfe582aa0288d
Jeff Hammond <jeff.science@gmail.com>
2023-01-15 15:49:20 +0200

    comm and win too

--- source/mpi_attr.c
+++ source/mpi_attr.c
@@ -4,38 +4,54 @@
 #include "convert_handles.h"
 #include "convert_constants.h"
 
-void C_MPI_Type_get_name(int * type_f, char ** pname, int * resultlen, int * ierror)
-{
-    MPI_Datatype type = C_MPI_TYPE_F2C(*type_f);
-    char * name = *pname;
-    *ierror = MPI_Type_get_name(type, name, resultlen);
-    C_MPI_RC_FIX(*ierror);
+#define C_MPI_HANDLE_GET_NAME(Full,SHORT,Short)                                             \
+void C_MPI_##Short##_get_name(int * handle_f, char ** pname, int * resultlen, int * ierror) \
+{                                                                                           \
+    MPI_##Full handle = C_MPI_##SHORT##_F2C(*handle_f);                                     \
+    char * name = *pname;                                                                   \
+    *ierror = MPI_##Short##_get_name(handle, name, resultlen);                              \
+    C_MPI_RC_FIX(*ierror);                                                                  \
 }
+
+#define C_MPI_HANDLE_SET_NAME(Full,SHORT,Short)                                             \
+void C_MPI_##Short##_set_name(int * handle_f, char ** pname, int * ierror)                  \
+{                                                                                           \
+    MPI_##Full handle = C_MPI_##SHORT##_F2C(*handle_f);                                     \
+    char * name = *pname;                                                                   \
+    *ierror = MPI_##Short##_set_name(handle, name);                                         \
+    C_MPI_RC_FIX(*ierror);                                                                  \
+}
+
+#define CFI_MPI_HANDLE_GET_NAME(Full,SHORT,Short)                                                       \
+void CFI_MPI_##Short##_get_name(int * handle_f, CFI_cdesc_t * name_d, int * resultlen, int * ierror)    \
+{                                                                                                       \
+    MPI_##Full handle = C_MPI_##SHORT##_F2C(*handle_f);                                                 \
+    char * name = name_d -> base_addr;                                                                  \
+    *ierror = MPI_##Short##_get_name(handle, name, resultlen);                                          \
+    C_MPI_RC_FIX(*ierror);                                                                              \
+}
+
+#define CFI_MPI_HANDLE_SET_NAME(Full,SHORT,Short)                                                       \
+void CFI_MPI_##Short##_set_name(int * handle_f, CFI_cdesc_t * name_d, int * ierror)                     \
+{                                                                                                       \
+    MPI_##Full handle = C_MPI_##SHORT##_F2C(*handle_f);                                                 \
+    char * name = name_d -> base_addr;                                                                  \
+    *ierror = MPI_##Short##_set_name(handle, name);                                                     \
+    C_MPI_RC_FIX(*ierror);                                                                              \
+}
+
+C_MPI_HANDLE_GET_NAME(Comm,COMM,Comm)
+C_MPI_HANDLE_SET_NAME(Comm,COMM,Comm)
+C_MPI_HANDLE_GET_NAME(Datatype,TYPE,Type)
+C_MPI_HANDLE_SET_NAME(Datatype,TYPE,Type)
+C_MPI_HANDLE_GET_NAME(Win,WIN,Win)
+C_MPI_HANDLE_SET_NAME(Win,WIN,Win)
 
 #ifdef HAVE_CFI
-void CFI_MPI_Type_get_name(int * type_f, CFI_cdesc_t * name_d, int * resultlen, int * ierror)
-{
-    MPI_Datatype type = C_MPI_TYPE_F2C(*type_f);
-    char * name = name_d -> base_addr;
-    *ierror = MPI_Type_get_name(type, name, resultlen);
-    C_MPI_RC_FIX(*ierror);
-}
-#endif
-
-void C_MPI_Type_set_name(int * type_f, char ** pname, int * ierror)
-{
-    MPI_Datatype type = C_MPI_TYPE_F2C(*type_f);
-    char * name = *pname;
-    *ierror = MPI_Type_set_name(type, name);
-    C_MPI_RC_FIX(*ierror);
-}
-
-#ifdef HAVE_CFI
-void CFI_MPI_Type_set_name(int * type_f, CFI_cdesc_t * name_d, int * ierror)
-{
-    MPI_Datatype type = C_MPI_TYPE_F2C(*type_f);
-    char * name = name_d -> base_addr;
-    *ierror = MPI_Type_set_name(type, name);
-    C_MPI_RC_FIX(*ierror);
-}
+CFI_MPI_HANDLE_GET_NAME(Comm,COMM,Comm)
+CFI_MPI_HANDLE_SET_NAME(Comm,COMM,Comm)
+CFI_MPI_HANDLE_GET_NAME(Datatype,TYPE,Type)
+CFI_MPI_HANDLE_SET_NAME(Datatype,TYPE,Type)
+CFI_MPI_HANDLE_GET_NAME(Win,WIN,Win)
+CFI_MPI_HANDLE_SET_NAME(Win,WIN,Win)
 #endif
