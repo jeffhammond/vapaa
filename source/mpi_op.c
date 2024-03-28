576c461346b2c2f8aaf84713f0ecc44c1c13bc6a
Jeff Hammond <jeff.science@gmail.com>
2024-03-28 17:12:48 +0200

    fix uninitialized op_f issues with terrible hack

--- source/mpi_op.c
+++ source/mpi_op.c
@@ -5,10 +5,13 @@
 #include "convert_handles.h"
 #include "convert_constants.h"
 
+static int op_f_index = 0;
+
 void C_MPI_Op_create(MPI_User_function *user_fn, int * commute_f, int * op_f, int * ierror)
 {
     MPI_Op op = MPI_OP_NULL;
     *ierror = MPI_Op_create(user_fn, *commute_f, &op);
+    *op_f = op_f_index++;
     int rc = add_op_f2c(*op_f, op);
     if (rc != 1) printf("add_op_f2c failed\n");
     C_MPI_RC_FIX(*ierror);
