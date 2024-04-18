d0b5a614bff415566b53c17dbf09a8b3379956dc
Jeff Hammond <jeff.science@gmail.com>
2024-04-18 08:58:16 +0300

    alloc, create, free for win

--- source/mpi_win.c
+++ source/mpi_win.c
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 #include <mpi.h>
+#include "ISO_Fortran_binding.h"
+#include "convert_handles.h"
+#include "convert_constants.h"
 #include "vapaa_constants.h"
 
 #if 0
@@ -16,3 +19,32 @@ static int C_MPI_TRANSLATE_WIN_ASSERT(int f)
     return c;
 }
 #endif
+
+void C_MPI_Win_allocatee(MPI_Aint size, int disp_unit, int info_f, int comm_f, void ** baseptr, int * win_f, int * ierror)
+{
+    MPI_Win win = MPI_WIN_NULL;
+    MPI_Info info = C_MPI_INFO_F2C(info_f);
+    MPI_Comm comm = C_MPI_COMM_F2C(comm_f);
+    *ierror = MPI_Win_allocate(size, disp_unit, info, comm, baseptr, &win);
+    *win_f = MPI_Win_c2f(win);
+    C_MPI_RC_FIX(*ierror);
+}
+
+void C_MPI_Win_create(void * base, MPI_Aint size, int disp_unit, int info_f, int comm_f, int * win_f, int * ierror)
+{
+    MPI_Win win = MPI_WIN_NULL;
+    MPI_Info info = C_MPI_INFO_F2C(info_f);
+    MPI_Comm comm = C_MPI_COMM_F2C(comm_f);
+    *ierror = MPI_Win_create(base, size, disp_unit, info, comm, &win);
+    *win_f = MPI_Win_c2f(win);
+    C_MPI_RC_FIX(*ierror);
+}
+
+void C_MPI_Win_free(int * win_f, int * ierror)
+{
+    MPI_Win win = C_MPI_WIN_F2C(*win_f);
+    *ierror = MPI_Win_free(&win);
+    *win_f = MPI_Win_c2f(win);
+    C_MPI_RC_FIX(*ierror);
+}
+
--- source/mpi_win_c.F90
+++ source/mpi_win_c.F90
@@ -2,5 +2,38 @@
 
 module mpi_win_c
 
+    interface
+        subroutine C_MPI_Win_allocate(size, disp_unit, info, comm, baseptr, win, ierror) &
+                   bind(C,name="C_MPI_Win_allocate")
+            use iso_c_binding, only: c_int, c_size_t, c_ptr
+            implicit none
+            integer(kind=c_size_t), intent(in) :: size
+            integer(kind=c_int), intent(in) :: disp_unit, info, comm
+            type(c_ptr), intent(out) :: baseptr
+            integer(kind=c_int), intent(out) :: win, ierror
+        end subroutine C_MPI_Win_allocate
+    end interface
+
+    interface
+        subroutine C_MPI_Win_create(base, size, disp_unit, info, comm, win, ierror) &
+                   bind(C,name="C_MPI_Win_create")
+            use iso_c_binding, only: c_int, c_size_t, c_ptr
+            implicit none
+            type(c_ptr), intent(in) :: base
+            integer(kind=c_size_t), intent(in) :: size
+            integer(kind=c_int), intent(in) :: disp_unit, info, comm
+            integer(kind=c_int), intent(out) :: win, ierror
+        end subroutine C_MPI_Win_create
+    end interface
+
+    interface
+        subroutine C_MPI_Win_free(win, ierror) &
+                   bind(C,name="C_MPI_Win_free")
+            use iso_c_binding, only: c_int
+            implicit none
+            integer(kind=c_int), intent(inout) :: win
+            integer(kind=c_int), intent(out) :: ierror
+        end subroutine C_MPI_Win_free
+    end interface
 
 end module mpi_win_c
