36da57e850a72dcaab830223183cec7629e8f321
Jeff Hammond <jeff.science@gmail.com>
2024-04-18 09:56:20 +0300

    alloc, free - untested
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- source/mpi_win.c
+++ source/mpi_win.c
@@ -20,7 +20,7 @@ static int C_MPI_TRANSLATE_WIN_ASSERT(int f)
 }
 #endif
 
-void C_MPI_Win_allocatee(MPI_Aint size, int disp_unit, int info_f, int comm_f, void ** baseptr, int * win_f, int * ierror)
+void C_MPI_Win_allocate(size_t size, int disp_unit, int info_f, int comm_f, void ** baseptr, int * win_f, int * ierror)
 {
     MPI_Win win = MPI_WIN_NULL;
     MPI_Info info = C_MPI_INFO_F2C(info_f);
@@ -30,7 +30,23 @@ void C_MPI_Win_allocatee(MPI_Aint size, int disp_unit, int info_f, int comm_f, v
     C_MPI_RC_FIX(*ierror);
 }
 
-void C_MPI_Win_create(void * base, MPI_Aint size, int disp_unit, int info_f, int comm_f, int * win_f, int * ierror)
+#ifdef HAVE_CFI
+void CFI_MPI_Win_allocate(size_t size, int disp_unit, int info_f, int comm_f, CFI_cdesc_t * desc, int * win_f, int * ierror)
+{
+    MPI_Win win = MPI_WIN_NULL;
+    MPI_Info info = C_MPI_INFO_F2C(info_f);
+    MPI_Comm comm = C_MPI_COMM_F2C(comm_f);
+    if (1 == CFI_is_contiguous(desc)) {
+        *ierror = MPI_Win_allocate(size, disp_unit, info, desc->base_addr, comm, &win);
+    } else {
+        VAPAA_Assert_msg(0,"The base argument to MPI_Win_allocate must be simply contiguous!");
+    }
+    *win_f = MPI_Win_c2f(win);
+    C_MPI_RC_FIX(*ierror);
+}
+#endif
+
+void C_MPI_Win_create(void * base, size_t size, int disp_unit, int info_f, int comm_f, int * win_f, int * ierror)
 {
     MPI_Win win = MPI_WIN_NULL;
     MPI_Info info = C_MPI_INFO_F2C(info_f);
@@ -41,7 +57,7 @@ void C_MPI_Win_create(void * base, MPI_Aint size, int disp_unit, int info_f, int
 }
 
 #ifdef HAVE_CFI
-void CFI_MPI_Win_create(CFI_cdesc_t * desc, MPI_Aint size, int disp_unit, int info_f, int comm_f, int * win_f, int * ierror)
+void CFI_MPI_Win_create(CFI_cdesc_t * desc, size_t size, int disp_unit, int info_f, int comm_f, int * win_f, int * ierror)
 {
     MPI_Win win = MPI_WIN_NULL;
     MPI_Info info = C_MPI_INFO_F2C(info_f);
@@ -64,3 +80,27 @@ void C_MPI_Win_free(int * win_f, int * ierror)
     C_MPI_RC_FIX(*ierror);
 }
 
+void C_MPI_Alloc_mem(size_t size, int info_f, void ** baseptr, int * ierror)
+{
+    MPI_Info info = C_MPI_INFO_F2C(info_f);
+    *ierror = MPI_Alloc_mem(size, info, baseptr);
+    C_MPI_RC_FIX(*ierror);
+}
+
+void C_MPI_Free_mem(void * baseptr, int * ierror)
+{
+    *ierror = MPI_Free_mem(baseptr);
+    C_MPI_RC_FIX(*ierror);
+}
+
+#if HAVE_CFI
+void CFI_MPI_Free_mem(CFI_cdesc_t * desc, int * ierror)
+{
+    if (1 == CFI_is_contiguous(desc)) {
+        *ierror = MPI_Free_mem(desc->base_addr);
+    } else {
+        VAPAA_Assert_msg(0,"The base argument to MPI_Free_mem must be simply contiguous!");
+    }
+    C_MPI_RC_FIX(*ierror);
+}
+#endif
--- source/mpi_win_c.F90
+++ source/mpi_win_c.F90
@@ -3,21 +3,36 @@
 module mpi_win_c
 
     interface
-        subroutine C_MPI_Win_allocate(size, disp_unit, info, comm, baseptr, win, ierror) &
+        subroutine C_MPI_Win_allocate(size, disp_unit, info, comm, base, win, ierror) &
                    bind(C,name="C_MPI_Win_allocate")
-            use iso_c_binding, only: c_int, c_size_t, c_ptr
+            use iso_c_binding, only: c_int, c_size_t
             implicit none
             integer(kind=c_size_t), intent(in), value :: size
             integer(kind=c_int), intent(in), value :: disp_unit, info, comm
-            type(c_ptr), intent(out) :: baseptr
+!dir$ ignore_tkr base
+            integer, dimension(*), intent(out), asynchronous :: base
             integer(kind=c_int), intent(out) :: win, ierror
         end subroutine C_MPI_Win_allocate
     end interface
 
+#ifdef HAVE_CFI
+    interface
+        subroutine CFI_MPI_Win_allocate(size, disp_unit, info, comm, base, win, ierror) &
+                   bind(C,name="CFI_MPI_Win_allocate")
+            use iso_c_binding, only: c_int, c_size_t, c_ptr
+            implicit none
+            integer(kind=c_size_t), intent(in), value :: size
+            integer(kind=c_int), intent(in), value :: disp_unit, info, comm
+            type(*), dimension(..), asynchronous :: base
+            integer(kind=c_int), intent(out) :: win, ierror
+        end subroutine C_MPI_Win_allocate
+    end interface
+#endif
+
     interface
         subroutine C_MPI_Win_create(base, size, disp_unit, info, comm, win, ierror) &
                    bind(C,name="C_MPI_Win_create")
-            use iso_c_binding, only: c_int, c_size_t, c_ptr
+            use iso_c_binding, only: c_int, c_size_t
             implicit none
 !dir$ ignore_tkr base
             integer, dimension(*), intent(in), asynchronous :: base
@@ -50,5 +65,40 @@ module mpi_win_c
             integer(kind=c_int), intent(out) :: ierror
         end subroutine C_MPI_Win_free
     end interface
+
+    interface
+        subroutine C_MPI_Alloc_mem(size, info, baseptr, ierror) &
+                   bind(C,name="C_MPI_Alloc_mem")
+            use iso_c_binding, only: c_int, c_size_t, c_ptr
+            implicit none
+            integer(kind=c_size_t), intent(in), value :: size
+            integer(kind=c_int), intent(in), value :: info
+            type(c_ptr), intent(out) :: baseptr
+            integer(kind=c_int), intent(out) :: ierror
+        end subroutine C_MPI_Alloc_mem
+    end interface
+
+    interface
+        subroutine C_MPI_Free_mem(baseptr, ierror) &
+                   bind(C,name="C_MPI_Free_mem")
+            use iso_c_binding, only: c_int
+            implicit none
+!dir$ ignore_tkr baseptr
+            integer, dimension(*), intent(in), asynchronous :: baseptr
+            integer(kind=c_int), intent(out) :: ierror
+        end subroutine C_MPI_Free_mem
+    end interface
+
+#if HAVE_CFI
+    interface
+        subroutine CFI_MPI_Free_mem(baseptr, ierror) &
+                   bind(C,name="CFI_MPI_Free_mem")
+            use iso_c_binding, only: c_int
+            implicit none
+            type(*), dimension(..), intent(in), asynchronous :: baseptr
+            integer(kind=c_int), intent(out) :: ierror
+        end subroutine CFI_MPI_Free_mem
+    end interface
+#endif
 
 end module mpi_win_c
