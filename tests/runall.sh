d9da3f02541dc9c166cace1d5879c7588834b40a
Jeff Hammond <jeff.science@gmail.com>
2026-04-13 13:20:53 +0300

    update build

--- tests/runall.sh
+++ tests/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #MPIRUN=$MPI_ROOT/bin/mpirun
-MPIRUN=/opt/homebrew/Cellar/mpich/4.3.1/bin/mpirun
+MPIRUN=/opt/homebrew/Cellar/mpich/5.0.1/bin/mpirun
 
 for f in `ls -1 *.x` ; do
     echo "*************************************************************"
